e80c6ac0d2de8a78adb80d26ec216ad8cd2a72c7
Maximilian Fickert <fickert@cs.uni-saarland.de>
2020-07-23 10:51:15 +0200

    added option to start minimized

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace HueLock {
 	/// <summary>
@@ -11,8 +12,12 @@ namespace HueLock {
 		protected override void OnStartup(StartupEventArgs e) {
 			base.OnStartup(e);
 
+			var minimized = Array.Exists(e.Args, arg => arg == "/minimized");
+
 			manager = new HueLockManager();
 			var MainWindow = new MainWindow(manager);
+			if (minimized)
+				MainWindow.WindowState = WindowState.Minimized;
 			MainWindow.Show();
 		}
 	}
